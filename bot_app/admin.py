663eea3690b758ba1131458fe8a91757ba70f3e5
muhdhisham <muhdhisham034@gmail.com>
2021-12-13 07:17:10 +0530

    Added Models and required libraries

--- bot_app/admin.py
+++ bot_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(TweetLookUpWord)
+admin.site.register(TweetLookUpBadWord)
+admin.site.register(OutBoundDirectMessage)
+admin.site.register(TweetLookUpCoordinates)
--- bot_app/models.py
+++ bot_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class TweetLookUpWord(models.Model):
+    keyword = models.CharField(max_length=255)
+
+class TweetLookUpBadWord(models.Model):
+    keyword = models.CharField(max_length=255)
+    
+class TweetLookUpCoordinates(models.Model):
+    value = models.TextField()
+
+class OutBoundDirectMessage(models.Model):
+    message = models.TextField()
+    
+class LastFollower(models.Model):
+    last_follower_id = models.PositiveBigIntegerField()
+    
+class InBoundDirectMessage(models.Model):
+    sender = models.CharField(max_length=255)
+    message = models.TextField()
+    sent_on = models.DateTimeField()
+    
+
+        
